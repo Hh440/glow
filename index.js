6a942f6cd590c3ca71dfbe27a87cb5c2ed60c888
Harsh <91595810+Hh440@users.noreply.github.com>
2024-09-16 18:12:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
